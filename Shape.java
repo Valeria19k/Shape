bf1f8f5c728d7b199a5f8eb67c3e4c96e51a2b8e
Valeria19k <71520455+Valeria19k@users.noreply.github.com>
2020-12-09 15:32:09 +0300

    Create Shape.java

---
+++ Shape.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public class Shape {
+    private String name;
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String toString() {
+        return " Фигура готова!";
+    }
+}
